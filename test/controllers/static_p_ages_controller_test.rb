2006baf4e6facd52a4e561b128d2bf5060f877e5
Jorge Fernando Rincón Figueroa <jofer86@gmail.com>
2019-05-08 17:40:26 -0500

    Some testing done,

--- test/controllers/static_p_ages_controller_test.rb
+++ test/controllers/static_p_ages_controller_test.rb
@@ -5,16 +5,10 @@ class StaticPAgesControllerTest < ActionDispatch::IntegrationTest
     @base_title = "Ruby on Rails Tutorial Sample App"
   end
 
-  test "should get root" do
-    get root_url
-    assert_response :success
-    assert_select "title", "Home | #{@base_title}"
-  end
-
   test "should get home" do
     get static_p_ages_home_url
     assert_response :success
-    assert_select "title", "Home | #{@base_title}"
+    assert_select "title", "Ruby on Rails Tutorial Sample App"
   end
 
   test "should get help" do
