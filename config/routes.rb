2efcfb8394cfec138f65f25507914ec2b68bbcc0
Jorge Fernando Rincón Figueroa <jofer86@gmail.com>
2019-05-12 14:52:16 -0500

    finished layout for home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
 
-  get 'static_p_ages/home'
-  get 'static_p_ages/help'
-  get 'static_p_ages/about'
-  get 'static_p_ages/contact'
+ 
+  get '/help', to: 'static_p_ages#help'
+  get '/about', to: 'static_p_ages#about'
+  get '/contact', to: 'static_p_ages#contact'
   root 'static_p_ages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
